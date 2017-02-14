50dc4227bc326a74b06a1ddbeef9b9d47b74db0e
Inna-Badiul <inna.badiul@gmail.com>
2017-02-14 16:23:36 +0100

    added changing the text function inside a result component

--- js/main.js
+++ js/main.js
@@ -1,18 +1,30 @@
-var checkboxes = document.getElementsByClassName("default-checkbox");
+document.addEventListener("DOMContentLoaded", function (event) {
+    var checkboxes = document.getElementsByClassName("default-checkbox");
+    var qs = document.querySelector.bind(document),
+        $buyNowButton = qs(".button"),
+        $selectedItemTitle = qs(".selected-item-title");
 
-for(var i = 0; i < checkboxes.length; i++) {
-    checkboxes[i].addEventListener("change", function () {
-        if (this.checked == false) {
-            this.checked = true;
-        } else {
-            var checkedItems = document.querySelectorAll('input:checked');
-            this.parentNode.className += " active";
-            for (var j = 0; j < checkedItems.length; j++) {
-                if(this !== checkedItems[j]){
-                    checkedItems[j].checked = false;
-                    checkedItems[j].parentNode.className = checkedItems[j].parentNode.className.replace("active","");
+    for (var i = 0; i < checkboxes.length; i++) {
+        checkboxes[i].addEventListener("change", function () {
+            if (this.checked == false) {
+                this.checked = true;
+            } else {
+                var checkedItems = document.querySelectorAll("input:checked");
+                this.parentNode.className += " active";
+                for (var j = 0; j < checkedItems.length; j++) {
+                    if (this !== checkedItems[j]) {
+                        checkedItems[j].checked = false;
+                        checkedItems[j].parentNode.className = checkedItems[j].parentNode.className.replace("active", "");
+                    }
                 }
+            }
+        });
+    }
+    $buyNowButton.addEventListener("click", function () {
+        for (var k = 0; k < checkboxes.length; k++) {
+            if (checkboxes[k].checked == true) {
+               $selectedItemTitle.innerText = checkboxes[k].parentNode.querySelector(".item-title").innerHTML;
             }
         }
     });
-}
+});
