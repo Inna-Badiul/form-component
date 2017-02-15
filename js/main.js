94e0d9e4e312556191cf6c8108b4aa1ddb866554
Inna-Badiul <inna.badiul@gmail.com>
2017-02-15 18:57:53 +0100

    added the proper colors to the component

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function (event) {
+document.addEventListener("DOMContentLoaded", function () {
     var $checkboxes = document.getElementsByClassName("default-checkbox");
     var qs = document.querySelector.bind(document),
         $buyNowButton = qs(".button"),
@@ -6,29 +6,36 @@ document.addEventListener("DOMContentLoaded", function (event) {
         $formWrapper = qs(".form-wrapper"),
         $resultWrapper = qs(".result-wrapper");
 
-    for (var i = 0; i < $checkboxes.length; i++) {
-        $checkboxes[i].addEventListener("change", function () {
-            if (this.checked == false) {
-                this.checked = true;
-            } else {
-                var checkedItems = document.querySelectorAll("input:checked");
-                this.parentNode.className += " active";
-                for (var j = 0; j < checkedItems.length; j++) {
-                    if (this !== checkedItems[j]) {
-                        checkedItems[j].checked = false;
-                        checkedItems[j].parentNode.className = checkedItems[j].parentNode.className.replace("active", "");
+    function toggleCheckedItem() {
+        for (var i = 0; i < $checkboxes.length; i++) {
+            $checkboxes[i].addEventListener("change", function () {
+                if (this.checked == false) {
+                    this.checked = true;
+                } else {
+                    var checkedItems = document.querySelectorAll("input:checked");
+                    this.parentNode.className += " active";
+                    for (var j = 0; j < checkedItems.length; j++) {
+                        if (this !== checkedItems[j]) {
+                            checkedItems[j].checked = false;
+                            checkedItems[j].parentNode.className = checkedItems[j].parentNode.className.replace("active", "");
+                        }
                     }
                 }
+            });
+        }
+    }
+    toggleCheckedItem();
+
+    function showResult() {
+        $buyNowButton.addEventListener("click", function () {
+            for (var k = 0; k < $checkboxes.length; k++) {
+                if ($checkboxes[k].checked == true) {
+                    $selectedItemTitle.innerText = $checkboxes[k].parentNode.querySelector(".item-title").innerHTML;
+                }
             }
+            $formWrapper.className += " hide-form";
+            $resultWrapper.className += " show-result";
         });
     }
-    $buyNowButton.addEventListener("click", function () {
-        for (var k = 0; k < $checkboxes.length; k++) {
-            if ($checkboxes[k].checked == true) {
-                $selectedItemTitle.innerText = $checkboxes[k].parentNode.querySelector(".item-title").innerHTML;
-            }
-        }
-        $formWrapper.className += " hide-form";
-        $resultWrapper.className += " show-result";
-    });
+    showResult();
 });
